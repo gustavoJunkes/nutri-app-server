9ddbf829d09411cbfaba95a46d8f4936d613aa39
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2022-11-22 20:39:04 -0300

    Create TokenAuthenticationProvider.java

---
+++ src/main/java/com/nutriapp/auth/TokenAuthenticationProvider.java
@@ -0,0 +1,32 @@
+package com.nutriapp.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.AbstractUserDetailsAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+final class TokenAuthenticationProvider extends AbstractUserDetailsAuthenticationProvider {
+
+    UserAuthenticationService auth;
+
+    @Autowired
+    public void setAuth(UserAuthenticationService auth) {
+        this.auth = auth;
+    }
+
+    @Override
+    protected void additionalAuthenticationChecks(final UserDetails d, final UsernamePasswordAuthenticationToken auth) {
+    }
+
+    @Override
+    protected UserDetails retrieveUser(final String username, final UsernamePasswordAuthenticationToken authentication) {
+        final Object token = authentication.getCredentials();
+        return Optional.ofNullable(token).map(String::valueOf).flatMap(auth::findByToken)
+                .orElseThrow(() -> new UsernameNotFoundException("Couldn't find user: " + token));
+    }
+}
