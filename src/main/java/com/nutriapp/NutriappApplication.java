7589a19a7aaa64cfd31e660dcddff796aaccec6b
Gustavo Junkes <gustavo.junkes@senior.com.br>
2022-12-08 14:11:48 -0300

    Add project via upload.

---
+++ src/main/java/com/nutriapp/NutriappApplication.java
@@ -0,0 +1,13 @@
+package com.nutriapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutriappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutriapp/NutriappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutriapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
