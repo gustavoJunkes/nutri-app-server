6c86f8214afab4a5a2b2d5e48491d00e9e55531d
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2022-12-10 11:02:17 -0300

    Update Exercise.java

--- src/main/java/com/nutriapp/domain/Exercise.java
+++ src/main/java/com/nutriapp/domain/Exercise.java
@@ -1,10 +1,16 @@
 package com.nutriapp.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Exercise {
-    String description;
-    double intensity;
-    double time;
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    private String description;
+    private double intensity;
+    private double time;
 }
