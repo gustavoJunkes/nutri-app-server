81099218e76f248181cd5b4a588a5c1937a77150
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2022-12-11 17:06:12 -0300

    Update NutriappApplication.java

--- src/main/java/com/nutriapp/NutriappApplication.java
+++ src/main/java/com/nutriapp/NutriappApplication.java
@@ -2,12 +2,16 @@ package com.nutriapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NutriappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NutriappApplication.class, args);
+
 	}
+
 
 }
