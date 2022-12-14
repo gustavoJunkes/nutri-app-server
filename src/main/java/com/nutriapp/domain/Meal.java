f0e7915532a2f7ecd60a4f1009addb5f686afd71
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2022-12-14 13:29:19 -0300

    Update Meal.java

--- src/main/java/com/nutriapp/domain/Meal.java
+++ src/main/java/com/nutriapp/domain/Meal.java
@@ -1,5 +1,32 @@
 package com.nutriapp.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.Instant;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
 public class Meal {
-    private
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @OneToMany
+    private Set<MealFood> mealFoods;
+
+    private LocalTime mealTime;
+    private DayTimeEnum mealPeriod;
 }
