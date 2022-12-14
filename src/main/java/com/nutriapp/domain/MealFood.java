fc0c3aa4c48ea01bd310bc3f1cf2398fc6e45b39
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2022-12-14 13:29:21 -0300

    Update MealFood.java

--- src/main/java/com/nutriapp/domain/MealFood.java
+++ src/main/java/com/nutriapp/domain/MealFood.java
@@ -1,6 +1,35 @@
 package com.nutriapp.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class MealFood {
 
-    
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private UUID id;
+
+    @ManyToOne
+    private Food food;
+
+    private double quantity; // gram
+
+    public MealFood(Food food, double calories) {
+        // calculate the quantity for the given calories
+        this.food = food;
+        this.quantity = calories / food.getCaloriesPerGram();
+    }
 }
