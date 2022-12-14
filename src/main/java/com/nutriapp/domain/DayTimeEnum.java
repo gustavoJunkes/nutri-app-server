4801f398a3c4f3902695aef764eadbbc46a0398a
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2022-12-14 13:29:11 -0300

    Create DayTimeEnum.java

---
+++ src/main/java/com/nutriapp/domain/DayTimeEnum.java
@@ -0,0 +1,13 @@
+package com.nutriapp.domain;
+
+public enum DayTimeEnum {
+    MORNING_EARLY,
+    BREAKFAST,
+    MORNING_LATE,
+    LUNCH,
+    AFTERNOON_EARLY,
+    AFTERNOON_LATE,
+    NIGHT_EARLY,
+    DINNER,
+    NIGHT_LATE
+}
