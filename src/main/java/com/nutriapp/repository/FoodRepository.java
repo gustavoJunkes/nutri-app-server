bae21b8a716cc456d7aaca20ab964be422b045fe
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2022-12-16 14:09:25 -0300

    Create FoodRepository.java

---
+++ src/main/java/com/nutriapp/repository/FoodRepository.java
@@ -0,0 +1,10 @@
+package com.nutriapp.repository;
+
+import com.nutriapp.domain.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food, Long> {
+
+}
