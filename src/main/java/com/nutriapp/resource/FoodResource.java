3b71c8f7cbcdfdff880f40670e6965f1b3562024
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2022-12-19 14:09:58 -0300

    Create FoodResource.java

---
+++ src/main/java/com/nutriapp/resource/FoodResource.java
@@ -0,0 +1,17 @@
+package com.nutriapp.resource;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/food")
+@RestController
+@AllArgsConstructor
+public class FoodResource {
+
+
+
+
+
+}
