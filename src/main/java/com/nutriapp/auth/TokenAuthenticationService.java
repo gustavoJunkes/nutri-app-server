5b87e30d5ea8b4a4a95d04685d8dc5e3ed1505f1
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2023-01-12 20:17:05 -0300

    Create TokenAuthenticationService.java

---
+++ src/main/java/com/nutriapp/auth/TokenAuthenticationService.java
@@ -0,0 +1,53 @@
+package com.nutriapp.auth;
+
+import com.google.common.collect.ImmutableMap;
+
+import lombok.AllArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
+
+import static lombok.AccessLevel.PACKAGE;
+import static lombok.AccessLevel.PRIVATE;
+
+@Service
+final class TokenAuthenticationService implements UserAuthenticationService {
+
+    TokenService tokenService;
+
+    @Autowired
+    public void setTokenService(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+    private UserService users;
+
+    @Autowired
+    public void setUsers(UserService users) {
+        this.users = users;
+    }
+
+    @Override
+    public Optional<String> login(final String username, final String password) {
+        return users
+                .findByUsername(username)
+                .filter(user -> Objects.equals(password, user.getPassword()))
+                .map(user -> tokenService.newToken(ImmutableMap.of("username", username)));
+    }
+
+    @Override
+    public Optional<User> findByToken(final String token) {
+        System.out.println("$$$$$$$$$$$$$$$$$$$$ token: " + token);
+        return Optional
+                .of(tokenService.verify(token))
+                .map(map -> map.get("username"))
+                .flatMap(users::findByUsername);
+    }
+
+    @Override
+    public void logout(final User user) {
+    }
+}
