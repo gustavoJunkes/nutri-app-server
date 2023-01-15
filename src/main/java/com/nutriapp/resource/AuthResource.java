25156ddb3ef0719857eddda01b6b816d1d265764
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2023-01-15 20:17:39 -0300

    Create AuthResource.java

---
+++ src/main/java/com/nutriapp/resource/AuthResource.java
@@ -0,0 +1,56 @@
+package com.nutriapp.resource;
+
+import com.nutriapp.auth.TokenService;
+import com.nutriapp.auth.User;
+import com.nutriapp.auth.UserService;
+import com.nutriapp.dto.AuthenticateDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/auth")
+public class AuthResource {
+
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    public void setAuthenticationManager(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    private TokenService tokenService;
+
+    @Autowired
+    public void setTokenService(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> authenticate(AuthenticateDto request) {
+        this.authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword())
+        );
+
+        User user = (User) userService.loadUserByUsername(request.getUsername());
+
+        if (user != null) {
+            return ResponseEntity.of(Optional.of(tokenService.generateToken(user)));
+        }
+        return ResponseEntity.status(401).body("It was not possible to authenticate");
+    }
+}
