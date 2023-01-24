29483e7278417c517daa5104375fe6c0e9485d45
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2023-01-23 22:50:48 -0300

    Update UserAuthenticationService.java

--- src/main/java/com/nutriapp/auth/UserAuthenticationService.java
+++ src/main/java/com/nutriapp/auth/UserAuthenticationService.java
@@ -1,11 +1,11 @@
-package com.nutriapp.auth;
-
-import com.nutriapp.auth.User;
-
-import java.util.Optional;
-
-public interface UserAuthenticationService {
-    Optional<String> login(String username, String password);
-    Optional<User> findByToken(String token);
-    void logout(User user);
-}
+//package com.nutriapp.auth;
+//
+//import com.nutriapp.auth.User;
+//
+//import java.util.Optional;
+//
+//public interface UserAuthenticationService {
+//    Optional<String> login(String username, String password);
+//    Optional<User> findByToken(String token);
+//    void logout(User user);
+//}
