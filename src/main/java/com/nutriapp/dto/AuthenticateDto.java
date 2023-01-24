6b0dbd547b6f03ae5944eacdcc991ec3ece50af3
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2023-01-23 22:51:02 -0300

    Update AuthenticateDto.java

--- src/main/java/com/nutriapp/dto/AuthenticateDto.java
+++ src/main/java/com/nutriapp/dto/AuthenticateDto.java
@@ -1,10 +1,12 @@
 package com.nutriapp.dto;
 
+import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
+@Builder
 public class AuthenticateDto {
     private String username;
     private String password;
