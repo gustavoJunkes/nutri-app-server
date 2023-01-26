2c7d52b4dc40675caa167ca218c5c68de0247df0
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2023-01-26 20:39:39 -0300

    Update UserRepository.java

--- src/main/java/com/nutriapp/repository/UserRepository.java
+++ src/main/java/com/nutriapp/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.nutriapp.repository;
 
-import com.nutriapp.domain.Food;
-import com.nutriapp.domain.User;
+import com.nutriapp.auth.User;
+import com.nutriapp.domain.BusinessUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
 
 }
