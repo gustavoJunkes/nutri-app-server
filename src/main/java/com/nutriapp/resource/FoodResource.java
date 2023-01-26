41dfe0d5d8cea6a3eca434ef5a24362f8413d04e
Gustavo Junkes <87446204+gustavoJunkes@users.noreply.github.com>
2023-01-26 20:39:44 -0300

    Update FoodResource.java

--- src/main/java/com/nutriapp/resource/FoodResource.java
+++ src/main/java/com/nutriapp/resource/FoodResource.java
@@ -1,7 +1,11 @@
 package com.nutriapp.resource;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class FoodResource {
 
-
-
+    @GetMapping
+    public ResponseEntity<String> demo() {
+        return new ResponseEntity<>("hello", HttpStatus.OK);
+    }
 
 
 }
